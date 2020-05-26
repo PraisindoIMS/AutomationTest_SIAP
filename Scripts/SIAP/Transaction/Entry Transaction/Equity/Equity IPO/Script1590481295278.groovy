250b178f19df93bba18199e0a828ee5fe71417e1
LENOVO <LENOVO@FITRI.developement.praisindo.com>
2020-05-26 16:36:45 +0700

    commit EQ IPO

--- Scripts/SIAP/Transaction/Entry Transaction/Equity/Equity IPO/Script1590481295278.groovy
+++ Scripts/SIAP/Transaction/Entry Transaction/Equity/Equity IPO/Script1590481295278.groovy
@@ -26,63 +26,63 @@ WebUI.setEncryptedText(findTestObject('Page_Login Page/input_Password_Password')
 
 WebUI.sendKeys(findTestObject('Page_Login Page/input_Password_Password'), Keys.chord(Keys.ENTER))
 
-WebUI.doubleClick(findTestObject('Object Repository/Page_IMS/span_SIAP'))
+WebUI.doubleClick(findTestObject('Equity IPO/span_SIAP'))
 
-WebUI.doubleClick(findTestObject('Object Repository/Page_IMS/span_Transaction'))
+WebUI.doubleClick(findTestObject('Equity IPO/span_Transaction'))
 
-WebUI.doubleClick(findTestObject('Object Repository/Page_IMS/span_Entry Transaction'))
+WebUI.doubleClick(findTestObject('Equity IPO/span_Entry Transaction'))
 
-WebUI.doubleClick(findTestObject('Object Repository/Page_IMS/span_Equity'))
+WebUI.doubleClick(findTestObject('Equity IPO/span_Equity'))
 
-WebUI.click(findTestObject('Object Repository/Page_IMS/span_Equity IPO'))
+WebUI.click(findTestObject('Equity IPO/span_Equity IPO'))
 
-WebUI.click(findTestObject('Object Repository/Page_IMS/td_Product_ContentPlaceHolder1_ASPxRoundPanel1_cmbPFolio_B-1'))
+WebUI.click(findTestObject('Equity IPO/td_Product_ContentPlaceHolder1_ASPxRoundPanel1_cmbPFolio_B-1'))
 
-WebUI.click(findTestObject('Object Repository/Page_IMS/td_DPJASINDO'))
+WebUI.click(findTestObject('Equity IPO/td_DPJASINDO'))
 
-WebUI.click(findTestObject('Object Repository/Page_IMS/td_Instrument_ContentPlaceHolder1_ASPxRound_986fd4'))
+WebUI.click(findTestObject('Equity IPO/td_Instrument_ContentPlaceHolder1_ASPxRound_986fd4'))
 
-WebUI.click(findTestObject('Object Repository/Page_IMS/td_TESTEQ'))
+WebUI.click(findTestObject('Equity IPO/td_TESTEQ'))
 
-WebUI.click(findTestObject('Object Repository/Page_IMS/td_InvClass_ContentPlaceHolder1_ASPxRoundPa_7d86cd'))
+WebUI.click(findTestObject('Equity IPO/td_InvClass_ContentPlaceHolder1_ASPxRoundPa_7d86cd'))
 
-WebUI.click(findTestObject('Object Repository/Page_IMS/td_TRADING'))
+WebUI.click(findTestObject('Equity IPO/td_TRADING'))
 
-WebUI.click(findTestObject('Object Repository/Page_IMS/td_Broker_ContentPlaceHolder1_ASPxRoundPane_0797b0'))
+WebUI.click(findTestObject('Equity IPO/td_Broker_ContentPlaceHolder1_ASPxRoundPane_0797b0'))
 
-WebUI.click(findTestObject('Object Repository/Page_IMS/td_AMC'))
+WebUI.click(findTestObject('Equity IPO/td_AMC'))
 
-WebUI.setText(findTestObject('Object Repository/Page_IMS/input_Instruction No_ctl00ContentPlaceHolde_d20025'), 'EQ201')
+WebUI.setText(findTestObject('Equity IPO/input_Instruction No_ctl00ContentPlaceHolde_d20025'), 'EQ201')
 
-WebUI.click(findTestObject('Object Repository/Page_IMS/td_'))
+WebUI.click(findTestObject('Equity IPO/td_'))
 
-WebUI.click(findTestObject('Object Repository/Page_IMS/td_25'))
+WebUI.click(findTestObject('Equity IPO/td_25'))
 
 <<<<<<< HEAD
-WebUI.click(findTestObject('Page_IMS/td_'))
+WebUI.click(findTestObject('Equity IPO/td_'))
 =======
-WebUI.click(findTestObject('Object Repository/Page_IMS/td_May 2020SunMonTueWedThuFriSat18262728293_bbf064'))
+WebUI.click(findTestObject('Equity IPO/td_May 2020SunMonTueWedThuFriSat18262728293_bbf064'))
 >>>>>>> branch 'master' of https://github.com/PraisindoIMS/AutomationTest_SIAP.git
 
-WebUI.click(findTestObject('Object Repository/Page_IMS/td_26'))
+WebUI.click(findTestObject('Equity IPO/td_26'))
 
-WebUI.click(findTestObject('Object Repository/Page_IMS/td_'))
+WebUI.click(findTestObject('Equity IPO/td_'))
 
-WebUI.click(findTestObject('Object Repository/Page_IMS/td_27'))
+WebUI.click(findTestObject('Equity IPO/td_27'))
 
-WebUI.setText(findTestObject('Object Repository/Page_IMS/input_Unit Price_ctl00ContentPlaceHolder1AS_f7c703'), '2123.65')
+WebUI.setText(findTestObject('Equity IPO/input_Unit Price_ctl00ContentPlaceHolder1AS_f7c703'), '2123.65')
 
-WebUI.setText(findTestObject('Object Repository/Page_IMS/input_Quantity_ctl00ContentPlaceHolder1ASPx_e13826'), '50000')
+WebUI.setText(findTestObject('Equity IPO/input_Quantity_ctl00ContentPlaceHolder1ASPx_e13826'), '50000')
 
-WebUI.click(findTestObject('Object Repository/Page_IMS/td_No TaxPPnPPh23OldPrepaid Tax 29PAJAK TAK_d068ae'))
+WebUI.click(findTestObject('Equity IPO/td_No TaxPPnPPh23OldPrepaid Tax 29PAJAK TAK_d068ae'))
 
-WebUI.click(findTestObject('Object Repository/Page_IMS/td_PPH23'))
+WebUI.click(findTestObject('Equity IPO/td_PPH23'))
 
-WebUI.setText(findTestObject('Object Repository/Page_IMS/textarea_Remarks 2_ctl00ContentPlaceHolder1_fac11b'), 'testing EQ IPO')
+WebUI.setText(findTestObject('Equity IPO/textarea_Remarks 2_ctl00ContentPlaceHolder1_fac11b'), 'testing EQ IPO')
 
-WebUI.click(findTestObject('Object Repository/Page_IMS/div_Save'))
+WebUI.click(findTestObject('Equity IPO/div_Save'))
 
-WebUI.click(findTestObject('Object Repository/Page_IMS/input_Deal has been saved Deal No  EQ55_ctl_3b85a9'))
+WebUI.click(findTestObject('Equity IPO/input_Deal has been saved Deal No  EQ55_ctl_3b85a9'))
 
 WebUI.closeBrowser()
 
